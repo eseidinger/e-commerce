e5971af838394890ee512f8c33caa4a6fb81f032
Emanuel Seidinger <dev@eseidinger.de>
2025-10-03 20:21:17 +0200

    fix kubernetes deployments

--- e-commerce-web/build-deploy.sh
+++ e-commerce-web/build-deploy.sh
@@ -1,9 +1,9 @@
-docker build -t localhost:32000/ecommerce-micro:latest .
-docker push localhost:32000/ecommerce-micro:latest
+docker build -t localhost:32000/ecommerce-web:latest .
+docker push localhost:32000/ecommerce-web:latest
 
-kubectl delete ns e-commerce-micro
-kubectl create ns e-commerce-micro
+kubectl delete ns e-commerce-web
+kubectl create ns e-commerce-web
 
-kubectl apply -n e-commerce-micro -f k8s/postgres-statefulset.yaml
-kubectl wait -n e-commerce-micro --for=condition=available --timeout=60s statefulset/postgres
-kubectl apply -n e-commerce-micro -f k8s/deployment.yaml
+kubectl apply -n e-commerce-web -f k8s/postgres-statefulset.yaml
+kubectl wait -n e-commerce-web --for=condition=available --timeout=60s statefulset/postgres
+kubectl apply -n e-commerce-web -f k8s/deployment.yaml
---
+++ e-commerce-web/undeploy.sh
@@ -0,0 +1,1 @@
+kubectl delete ns e-commerce-web
