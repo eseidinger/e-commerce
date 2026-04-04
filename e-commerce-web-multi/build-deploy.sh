c53fac743c063a1f1c14068ca3cda6e49c0a123d
Emanuel Seidinger <dev@eseidinger.de>
2026-04-04 12:21:54 +0200

    remove kubernetes deployment from e-commerce-web-multi

--- e-commerce-web-multi/build-deploy.sh
+++
@@ -1,9 +0,0 @@
-docker build -t localhost:32000/ecommerce-web:latest .
-docker push localhost:32000/ecommerce-web:latest
-
-kubectl delete ns e-commerce-web
-kubectl create ns e-commerce-web
-
-kubectl apply -n e-commerce-web -f k8s/postgres-statefulset.yaml
-kubectl wait -n e-commerce-web --for=condition=available --timeout=60s statefulset/postgres
-kubectl apply -n e-commerce-web -f k8s/deployment.yaml
