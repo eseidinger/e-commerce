36e0f05e8f42ac24406f527274d1ee4ce1978565
Emanuel Seidinger <dev@eseidinger.de>
2026-04-04 11:20:03 +0200

    change package names in e-commerce-web-single

---
+++ e-commerce-web-single/src/main/java/de/eseidinger/ecommerce/config/FlywayMigrationBean.java
@@ -0,0 +1,31 @@
+package de.eseidinger.ecommerce.config;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.ejb.Singleton;
+import jakarta.ejb.Startup;
+import org.flywaydb.core.Flyway;
+
+@Singleton
+@Startup
+public class FlywayMigrationBean {
+
+    @PostConstruct
+    public void migrate() {
+        try {
+            String dbHost = System.getenv().getOrDefault("DB_HOST", "localhost");
+            String dbUser = System.getenv().getOrDefault("JDBC_USER", "postgres");
+            String dbPassword = System.getenv().getOrDefault("JDBC_PASSWORD", "password");
+            Flyway flyway = Flyway.configure()
+                .dataSource(
+                    "jdbc:postgresql://" + dbHost + ":5432/ecommerce",
+                    dbUser,
+                    dbPassword
+                )
+                .locations("classpath:db/migration")
+                .load();
+            flyway.migrate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ e-commerce-web-single/src/main/java/de/eseidinger/ecommerce/exception/SecurityException.java
@@ -0,0 +1,7 @@
+package de.eseidinger.ecommerce.exception;
+
+public class SecurityException extends RuntimeException {
+  public SecurityException(String message) {
+    super(message);
+  }
+}
