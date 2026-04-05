b74297be57504fc4cb2e8233657d3873ded91d0e
Emanuel Seidinger <dev@eseidinger.de>
2026-04-05 17:22:24 +0200

    update kubernetes deployment

--- e-commerce-micro/build-deploy.sh
+++ e-commerce-micro/build-deploy.sh
@@ -1,9 +1,9 @@
 docker build -t localhost:32000/ecommerce-micro:latest .
 docker push localhost:32000/ecommerce-micro:latest
 
-kubectl delete ns e-commerce-micro
-kubectl create ns e-commerce-micro
+microk8s.kubectl delete ns e-commerce-micro
+microk8s.kubectl create ns e-commerce-micro
 
-kubectl apply -n e-commerce-micro -f k8s/postgres-statefulset.yaml
-kubectl wait -n e-commerce-micro --for=condition=available --timeout=60s statefulset/postgres
-kubectl apply -n e-commerce-micro -f k8s/deployment.yaml
+microk8s.kubectl apply -n e-commerce-micro -f k8s/postgres-statefulset.yaml
+microk8s.kubectl wait -n e-commerce-micro --for=condition=available --timeout=60s statefulset/postgres
+microk8s.kubectl apply -n e-commerce-micro -f k8s/deployment.yaml
